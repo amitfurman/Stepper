098abf56e62eb7406adfcb93d610ded15a0ad19b
amitfu <amitff103@gmail.com>
2023-05-16 15:27:01 +0300

    starting javaFX

---
+++ JavaFX/src/javafx/Controller.java
@@ -0,0 +1,39 @@
+package javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controller {
+
+    private String filePath;
+    @FXML
+    private Button chooseXMLFileButton;
+    @FXML
+    private TextField chosenXmlFilePath;
+
+    @FXML
+    void clickToChooseXMLFileButton(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose XML File");
+
+        Stage stage = (Stage) chooseXMLFileButton.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile != null) {
+            filePath = selectedFile.getAbsolutePath();
+        }
+    }
+
+    @FXML
+    void viewChosenXmlFilePath(ActionEvent event) {
+        chosenXmlFilePath.setText(filePath);
+
+    }
+
+}
