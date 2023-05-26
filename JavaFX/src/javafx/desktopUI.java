ce780b5d9ebd4399fb031f514902de14b41eaec3
amitfu <amitff103@gmail.com>
2023-05-26 16:33:16 +0300

    before table

--- JavaFX/src/javafx/desktopUI.java
+++ JavaFX/src/javafx/desktopUI.java
@@ -2,6 +2,7 @@ package javafx;
 
 import javafx.application.Application;
 import javafx.flowDefinitionTab.FlowDefinitionTabController;
+import javafx.flowExecutionTab.FlowExecutionTabController;
 import javafx.fxml.FXMLLoader;
 import javafx.header.HeaderController;
 import javafx.scene.Scene;
@@ -9,7 +10,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -29,13 +29,7 @@ public class desktopUI extends Application {
         fxmlLoader.setLocation(url);
         GridPane headerComponent = fxmlLoader.load(url.openStream());
         HeaderController headerController = fxmlLoader.getController();
-/*
-        fxmlLoader = new FXMLLoader();
-        url = getClass().getResource("flowDefinitionTab/flowDefinitionTab.fxml");
-        fxmlLoader.setLocation(url);
-        AnchorPane flowDefinitionTabComponent = fxmlLoader.load(url.openStream());
-        FlowDefinitionTabController flowDefinitionTabController =  fxmlLoader.getController();
-*/
+
         fxmlLoader = new FXMLLoader();
         url = getClass().getResource("source.fxml");
         fxmlLoader.setLocation(url);
@@ -44,23 +38,30 @@ public class desktopUI extends Application {
 
         BorderPane borderPane = (BorderPane)mainComponent.getContent();
         borderPane.setTop(headerComponent);
-/*
-        TabPane tabPane1 = new TabPane();
-        Tab flowDefinitionTab = new Tab("Flow Definition");
-        flowDefinitionTab.setContent(flowDefinitionTabComponent);
-        tabPane1.getTabs().add(flowDefinitionTab);
 
-        BorderPane borderPane1 = new BorderPane();
-        borderPane.setCenter(mainComponent);
-        borderPane.setTop(tabPane1);
-*/
+        fxmlLoader = new FXMLLoader();
+        url = getClass().getResource("flowDefinitionTab/flowDefinitionTab.fxml");
+        fxmlLoader.setLocation(url);
+        Tab flowDefinitionTabComponent = fxmlLoader.load(url.openStream());
+        FlowDefinitionTabController flowDefinitionTabController =  fxmlLoader.getController();
+
+
+        fxmlLoader = new FXMLLoader();
+        url = getClass().getResource("flowExecutionTab/flowExecutionTab.fxml");
+        fxmlLoader.setLocation(url);
+        Tab flowExecutionTabComponent = fxmlLoader.load(url.openStream());
+        FlowExecutionTabController flowExecutionTabController =  fxmlLoader.getController();
+
+        TabPane tabPane = (TabPane) borderPane.getCenter();
+        tabPane.getTabs().addAll(flowDefinitionTabComponent,flowExecutionTabComponent);
+
+
         mainController.setHeaderComponentController(headerController);
-     //   mainController.setFlowDefinitionTabController(flowDefinitionTabController);
-
+        mainController.setFlowDefinitionTabController(flowDefinitionTabController);
+        mainController.setFlowExecutionTabController(flowExecutionTabController);
         Scene scene = new Scene(mainComponent, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
---
+++ JavaFX/src/javafx/flowExecutionTab/FlowExecutionTabController.java
@@ -0,0 +1,143 @@
+package javafx.flowExecutionTab;
+
+import dto.DTOSingleFlowIOData;
+import javafx.Controller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+import java.util.List;
+
+public class FlowExecutionTabController {
+    private Controller mainController;
+    @FXML
+    TableView freeInputsTable;
+    @FXML
+    private TableColumn<Input, String> nameColumn;
+
+    @FXML
+    private TableColumn<Input, String> necessityColumn;
+
+    @FXML
+    private TableColumn<Input, String> valueColumn;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    public void initialize() {
+        AnchorPane.setTopAnchor(borderPane, 0.0);
+        AnchorPane.setBottomAnchor(borderPane, 0.0);
+        AnchorPane.setLeftAnchor(borderPane, 0.0);
+        AnchorPane.setRightAnchor(borderPane, 0.0);
+
+        hbox.setPrefWidth(400); // Set an initial width for the HBox
+        freeInputsTable.prefWidthProperty().bind(hbox.widthProperty());
+
+        nameColumn.prefWidthProperty().bind(hbox.widthProperty().multiply(0.3));
+        necessityColumn.prefWidthProperty().bind(hbox.widthProperty().multiply(0.2));
+        valueColumn.prefWidthProperty().bind(hbox.widthProperty().multiply(0.5));
+
+        // Resize TextField with column width
+        valueColumn.setCellFactory(column -> {
+            return new TableCell<Input, String>() {
+                private final TextField textField = new TextField();
+                {
+                    textField.getStyleClass().add("text-field");
+                    textField.setPrefWidth(Double.MAX_VALUE);
+                    textField.setMaxWidth(Double.MAX_VALUE);
+
+                    textField.setOnAction(event -> {
+                        commitEdit(textField.getText());
+                    });
+
+                    textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+                        if (!isNowFocused) {
+                            commitEdit(textField.getText());
+                        }
+                    });
+                }
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        setText(null);
+                        textField.setText(item);
+                        setGraphic(textField);
+                    }
+                }
+
+                @Override
+                public void startEdit() {
+                    super.startEdit();
+
+                    if (!isEmpty()) {
+                        textField.setText(getItem());
+                        setGraphic(textField);
+                        setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                        textField.requestFocus();
+                    }
+                }
+
+                @Override
+                public void cancelEdit() {
+                    super.cancelEdit();
+
+                    setText(getItem());
+                    setGraphic(null);
+                    setContentDisplay(ContentDisplay.TEXT_ONLY);
+                }
+
+                @Override
+                public void commitEdit(String newValue) {
+                    super.commitEdit(newValue);
+
+                    Input item = (Input) getTableRow().getItem();
+                    item.setValue(newValue);
+
+                    setText(newValue);
+                    setGraphic(null);
+                    setContentDisplay(ContentDisplay.TEXT_ONLY);
+                    getTableView().refresh();
+                }
+
+            };
+        });
+    }
+
+
+    public void setMainController(Controller mainController) {
+        this.mainController = mainController;
+    }
+
+    public void initInputsTable(List<DTOSingleFlowIOData> freeInputs) {
+        ObservableList<Input> inputList = FXCollections.observableArrayList();
+
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        necessityColumn.setCellValueFactory(new PropertyValueFactory<>("mandatory"));
+        valueColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
+
+        freeInputs.forEach(freeInput -> {
+            Input input = new Input();
+            input.setName(freeInput.getFinalName());
+            input.setMandatory(freeInput.getNecessity().toString());
+            input.setValue(freeInput.getValue());
+            inputList.add(input);
+        });
+
+        freeInputsTable.setEditable(true);
+        freeInputsTable.setItems(inputList);
+    }
+}
